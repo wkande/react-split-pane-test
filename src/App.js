2cc22e8bc3e74e2f06881c37ed1bf2ab90968465
Warren Anderson <warren@wyomingsoftware.com>
2021-01-15 08:53:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import SplitPane, { Pane } from 'react-split-pane';
 
 function App() {
+
+  //const elements = ['one', 'two', 'three'];
+
+  const items = []
+  items.push(<li key="x">Dude how will this wrap if it is to long</li>)
+  for(var i=0; i<300; i++){
+    items.push(<li key={i}>{i}</li>)
+  }
+
+
+
+  /*for (const [index, value] of elements.entries()) {
+    items.push(<li key={index}>{value}</li>)
+  }*/
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SplitPane split="vertical" defaultSize={300} allowResize={false}>
+        <Pane className="navPane" > 
+          <div className="paneScoller">
+            <br/>
+            <div className="NavCompanyName">Docula</div><br/>docs-like-code: simple
+            <hr/>
+            {items}
+          </div>
+        </Pane>
+ 
+        <Pane className="paneScoller">
+          Using a Pane allows you to specify any constraints directly
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          END
+        </Pane>
+      </SplitPane>
+      
     </div>
   );
 }
